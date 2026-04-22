c4616b4c34331b03a5ea3d3b523110ee68da6c66
qqqwwweezxc <vitos.burdash@gmail.com>
2026-04-22 16:36:24 +0100

    lesson 15 optional tasks

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path, re_path
-from .views import home, about, contact, post_view, profile_view, event_view
+from .views import (
+    home, about, contact, post_view, profile_view, event_view,
+    version_product, blog, order, report)
 
 
 urlpatterns = [
-    path('', home),
-    path('about/', about),
-    path('contact/', contact),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
     re_path(r'^post/(\d+)/$', post_view),
     re_path(r'^profile/([a-zA-Z]+)/$', profile_view),
     re_path(r'^event/(\d{4})/(\d{2})/(\d{2})/$', event_view),
-
+    re_path(r'^release/v(\d{1,2})$', version_product),
+    re_path(r'^blog/(\d{4})/(\d{2})$', blog),
+    re_path(r'^order/([A-Z]{2}\d{4})$', order),
+    re_path(r'^report/([a-zA-Z]+)(?:\.(pdf|csv))?$', report),
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,30 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, './home.html')
+    return render(request, 'home/home.html')
 
 
 def about(request):
-    return render(request, './about.html')
+    return render(request, 'home/about.html')
 
 
 def contact(request):
-    return render(request, './contact.html')
+    return render(request, 'home/contact.html')
 
 
 def post_view(request, id):
-    return render(request, './post.html', {'id': id})
+    return render(request, 'home/post.html', {'id': id})
 
 
 def profile_view(request, username):
-    return render(request, './profile.html', {'username': username})
+    return render(request, 'home/profile.html', {'username': username})
 
 
 def event_view(request, year, month, day):
-    return render(request, './event.html', {
+    return render(request, 'home/event.html', {
         'year': year,
         'month': month,
         'day': day
     })
+
+
+def version_product(request, version):
+    return render(request, 'home/version.html', {'version': version})
+
+
+def blog(request, year, month):
+    return render(request, 'home/blog.html', {'year': year, 'month': month})
+
+
+def order(request, order_id):
+    return render(request, 'home/order.html', {'order_id': order_id})
+
+
+def report(request, slug, ext: None):
+    return render(request, 'home/report.html', {'slug': slug, 'ext': ext})
