57508eafee1e1ce26260e1ba7ec6839b4d0ebad1
qqqwwweezxc <vitos.burdash@gmail.com>
2026-04-25 14:40:50 +0100

    lesson 16

--- home/urls.py
+++ home/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path, re_path
-from .views import (
-    home, about, contact, post_view, profile_view, event_view,
-    version_product, blog, order, report)
+from . import views
 
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    re_path(r'^post/(\d+)/$', post_view),
-    re_path(r'^profile/([a-zA-Z]+)/$', profile_view),
-    re_path(r'^event/(\d{4})/(\d{2})/(\d{2})/$', event_view),
-    re_path(r'^release/v(\d{1,2})$', version_product),
-    re_path(r'^blog/(\d{4})/(\d{2})$', blog),
-    re_path(r'^order/([A-Z]{2}\d{4})$', order),
-    re_path(r'^report/([a-zA-Z]+)(?:\.(pdf|csv))?$', report),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    re_path(r'^post/(\d+)/$', views.post_view),
+    re_path(r'^profile/([a-zA-Z]+)/$', views.profile_view),
+    re_path(r'^event/(\d{4})/(\d{2})/(\d{2})/$', views.event_view),
+    re_path(r'^release/v(\d{1,2})$', views.version_product),
+    re_path(r'^blog/(\d{4})/(\d{2})$', views.blog),
+    re_path(r'^order/([A-Z]{2}\d{4})$', views.order),
+    re_path(r'^report/([a-zA-Z]+)(?:\.(pdf|csv))?$', views.report),
 ]
 
--- my_site/settings.py
+++ my_site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'home/templates']
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
