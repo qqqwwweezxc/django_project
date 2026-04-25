fef369054e901c4b472c3d2b0a56a05adeff31da
qqqwwweezxc <vitos.burdash@gmail.com>
2026-04-25 14:50:24 +0100

    lesson 16 after changes

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',  views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
-    path('service/', views.ServiceView.as_view(), name='service'),
+    path('',  views.home, name='home_main'),
+    path('about/', views.about, name='about_main'),
+    path('contact/', views.ContactView.as_view(), name='contact_main'),
+    path('service/', views.ServiceView.as_view(), name='service_main'),
 ]
